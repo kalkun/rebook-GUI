8f019dac52e4072220effc238f91917bdfbedf7f
Rane Eriksen <rane@eriksen.be>
2014-04-09 16:25:12 +0200

    Almost done index - books

--- index.php
+++ index.php
@@ -2,168 +2,260 @@
 <html lang="en">
 <title>ReBook</title>
 <link rel="shortcut icon" href="/resources/icons/logo.ico"/>
-	<head>
-		<link rel="stylesheet" type="text/css" href="frontpage.css">
-		<link rel="stylesheet" type="text/css" href="general.css">
-		<link rel="stylesheet" type="text/css" href="index.css">
-	</head>
-	<body>
-		<div class="topbar">
-			<div class="home">
-				<div class="logo">
-				</div>
-				<div class="brand">
-					reBOOK
-				</div>
+<head>
+	<link rel="stylesheet" type="text/css" href="frontpage.css">
+	<link rel="stylesheet" type="text/css" href="general.css">
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div class="topbar">
+		<div class="home">
+			<div class="logo">
+			</div>
+			<div class="brand">
+				reBOOK
+			</div>
+		</div>
+		<div class="find">
+			FIND
+		</div>
+		<div class="sell">
+			SELL
+		</div>
+		<div class="about">
+			ABOUT
+		</div>
+		<div class="bugs">
+			BUGS
+		</div>
+		<input class="searchfield" type="text" placeholder="search">
+	</input>
+	<div class="user">
+		Jesper
+	</div>
+	<div class="edit">
+	</div>
+</div>
+<div class="banner">
+	<div class="header">
+		FIND AND SELL YOUR ACADEMIC BOOKS
+	</div>
+	<div class="subheader">
+		MADE FOR STUDENTS BY STUDENTS
+	</div>
+	<div class="price">
+		295 DKK
+	</div>
+	<div class="buttons">
+		<div>
+			<div id="sale">
+				654 books for sale
 			</div>
 			<div class="find">
-				FIND
+				FIND BOOKS
+			</div>
+		</div>
+		<div>
+			<div id="sold">
+				1002 books sold
 			</div>
 			<div class="sell">
-				SELL
-			</div>
-			<div class="about">
-				ABOUT
-			</div>
-			<div class="bugs">
-				BUGS
-			</div>
-			<input class="searchfield" type="text" placeholder="search">
-			</input>
-			<div class="user">
-				Jesper
-			</div>
-			<div class="edit">
-			</div>
-		</div>
-		<div class="banner">
-			<div class="header">
-				FIND AND SELL YOUR ACADEMIC BOOKS
-			</div>
-			<div class="subheader">
-				MADE FOR STUDENTS BY STUDENTS
-			</div>
-			<div class="price">
-				295 DKK
-			</div>
-			<div class="buttons">
-				<div>
-					<div id="sale">
-						654 books for sale
-					</div>
-					<div class="find">
-						FIND BOOKS
-					</div>
-				</div>
-				<div>
-					<div id="sold">
-						1002 books sold
-					</div>
-					<div class="sell">
-						SELL BOOKS
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="cleafix"></div>
-	<div class="tile-row">
-		<div class="tile">
-			<div class="title">
-				Java Setup
-			</div>
-			<div class="thumb">
-				<img src="resources/book.png">
-			</div>
-			<div class="price">
-				295 DKK
-			</div>
-			<div class="buttons">
-				<button>INFO</button>
-				<button>HANDSHAKE</button>
-			</div>
-		</div>
-		<div class="tile">
-			<div class="title">
-				Java Setup
-			</div>
-			<div class="thumb">
-				<img src="resources/book.png">
-			</div>
-			<div class="price">
-				295 DKK
-			</div>
-			<div class="buttons">
-				<button>INFO</button>
-				<button>HANDSHAKE</button>
-			</div>
-		</div>
-		<div class="tile">
-			<div class="title">
-				Java Setup
-			</div>
-			<div class="thumb">
-				<img src="resources/book.png">
-			</div>
-			<div class="price">
-				295 DKK
-			</div>
-			<div class="buttons">
-				<button>INFO</button>
-				<button>HANDSHAKE</button>
-			</div>
-		</div>
-	</div>
-	<div class="cleafix"></div>
-	<div class="tile-row">
-		<div class="tile">
-			<div class="title">
-				Java Setup
-			</div>
-			<div class="thumb">
-				<img src="resources/book.png">
-			</div>
-			<div class="price">
-				295 DKK
-			</div>
-			<div class="buttons">
-				<button>INFO</button>
-				<button>HANDSHAKE</button>
-			</div>
-		</div>
-		<div class="tile">
-			<div class="title">
-				Java Setup
-			</div>
-			<div class="thumb">
-				<img src="resources/book.png">
-			</div>
-			<div class="price">
-				295 DKK
-			</div>
-			<div class="buttons">
-				<button>INFO</button>
-				<button>HANDSHAKE</button>
-			</div>
-		</div>
-		<div class="tile">
-			<div class="title">
-				Java Setup
-			</div>
-			<div class="thumb">
-				<img src="resources/book.png">
-			</div>
-			<div class="price">
-				295 DKK
-			</div>
-			<div class="buttons">
-				<button>INFO</button>
-				<button>HANDSHAKE</button>
+				SELL BOOKS
 			</div>
 		</div>
 	</div>
 </div>
 <div class="cleafix"></div>
+<div class="tile-row">
+	<div class="tile">
+		<div class="title" title="Java Setup">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+</div>
+<div class="cleafix"></div>
+<div class="tile-row">
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+</div>
+<div class="tile-row">
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+</div>
+<div class="cleafix"></div>
+<div class="tile-row">
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+	<div class="tile">
+		<div class="title">
+			Java Setup
+		</div>
+		<div class="thumb">
+			<img src="resources/book.png">
+		</div>
+		<div class="price">
+			295 DKK
+		</div>
+		<div class="buttons">
+			<button>INFO</button>
+			<button>HANDSHAKE</button>
+		</div>
+	</div>
+</div>
 </div>
 <?php
 include "modules/modals/info.html";
