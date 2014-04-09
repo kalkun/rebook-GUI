227b8540c1528f43ea85031171c4cc5884ecd911
kalkun <jesper.henrichsen@gmail.com>
2014-04-09 13:18:34 +0200

    initial commit

---
+++ rebook.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>ReBook</title>
+<link rel="shortcut icon" href="/resources/icons/logo.ico"/>
+	<head>
+	</head>
+	<body>
+		<div class="topbar">
+			<div class="brand">
+			</div>
+			<div class="find">
+			</div>
+			<div class="sell">
+			</div>
+			<div class="about">
+			</div>
+			<div class="bugs">
+			</div>
+			<div class="searchfield">
+			</div>
+			<div class="user">
+			</div>
+			<div class="edit">
+			</div>
+		</div>
+		<div class="banner">
+			<div class="header">
+			</div>
+			<div class="subheader">
+			</div>
+			<div id="sale">
+			</div>
+			<div class="find">
+			</div>
+			<div id="sold">
+			</div>
+			<div class="sell">
+			</div>
+		</div>
+		<div class="body"> /**
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+			<div class="tile">
+				<div class="title">
+				</div>
+				<div class="thumb">
+				</div>
+				<div class="price">
+				</div>
+				<div class="info">
+				</div>
+				<div class="handshake">
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
