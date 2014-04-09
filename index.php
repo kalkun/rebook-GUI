13afab2aab736876293a2c2c7eea9bcb64daafe9
Rane Eriksen <rane@eriksen.be>
2014-04-09 15:24:39 +0200

    Added general.css

--- index.php
+++ index.php
@@ -2,165 +2,216 @@
 <html lang="en">
 <title>ReBook</title>
 <link rel="shortcut icon" href="/resources/icons/logo.ico"/>
-	<head>
-		<link rel="stylesheet" type="text/css" href="frontpage.css">
-	<!--	<link rel="stylesheet" type="text/css" href="modal.css"> -->
-	</head>
-	<body>
-		<div class="topbar">
-			<div class="home">
-				<div class="logo">
-				</div>
-				<div class="brand">
-					reBOOK
-				</div>
+<head>
+	<link rel="stylesheet" type="text/css" href="frontpage.css">
+	<link rel="stylesheet" type="text/css" href="modal.css">
+	<link rel="stylesheet" type="text/css" href="general.css">
+</head>
+<body>
+	<div class="topbar">
+		<div class="home">
+			<div class="logo">
 			</div>
-			<div class="find">
-				FIND
-			</div>
-			<div class="sell">
-				SELL
-			</div>
-			<div class="about">
-				ABOUT
-			</div>
-			<div class="bugs">
-				BUGS
-			</div>
-			<input class="searchfield" type="text" placeholder="search">
-			</input>
-			<div class="user">
-				Jesper
-			</div>
-			<div class="edit">
+			<div class="brand">
+				reBOOK
 			</div>
 		</div>
-		<div class="banner">
-			<div class="header">
+		<div class="find">
+			FIND
+		</div>
+		<div class="sell">
+			SELL
+		</div>
+		<div class="about">
+			ABOUT
+		</div>
+		<div class="bugs">
+			BUGS
+		</div>
+		<input class="searchfield" type="text" placeholder="search">
+	</input>
+	<div class="user">
+		Jesper
+	</div>
+	<div class="edit">
+	</div>
+</div>
+<div class="banner">
+	<div class="header">
+	</div>
+	<div class="subheader">
+	</div>
+	<div id="sale">
+	</div>
+	<div class="find">
+	</div>
+	<div id="sold">
+	</div>
+	<div class="sell">
+	</div>
+</div>
+<div class="body">
+	<div class="tile-row">
+		<div class="tile">
+			<div class="title">
+				Java Setup
 			</div>
-			<div class="subheader">
+			<div class="thumb">
+				<img src="">
 			</div>
-			<div id="sale">
+			<div class="price">
+				295 DKK
 			</div>
-			<div class="find">
+			<div class="info">
+				<button>INFO</button>
 			</div>
-			<div id="sold">
-			</div>
-			<div class="sell">
+			<div class="handshake">
+				<button>HANDSHAKE</button>
 			</div>
 		</div>
-		<div class="body"> 
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
+		<div class="tile">
+			<div class="title">
+				Java Setup
 			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
+			<div class="thumb">
+				<img src="">
 			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
+			<div class="price">
+				295 DKK
 			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
+			<div class="info">
+				<button>INFO</button>
 			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
-			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
-			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
-			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
-			</div>
-			<div class="tile">
-				<div class="title">
-				</div>
-				<div class="thumb">
-				</div>
-				<div class="price">
-				</div>
-				<div class="info">
-				</div>
-				<div class="handshake">
-				</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
 			</div>
 		</div>
-		<?php
-		include "modules/modals/info.html";
-		?>
-	</body>
+		<div class="tile">
+			<div class="title">
+				Java Setup
+			</div>
+			<div class="thumb">
+				<img src="">
+			</div>
+			<div class="price">
+				295 DKK
+			</div>
+			<div class="info">
+				<button>INFO</button>
+			</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
+			</div>
+		</div>
+	</div>
+	<div class="cleafix"></div>
+	<div class="tile-row">
+		<div class="tile">
+			<div class="title">
+				Java Setup
+			</div>
+			<div class="thumb">
+				<img src="">
+			</div>
+			<div class="price">
+				295 DKK
+			</div>
+			<div class="info">
+				<button>INFO</button>
+			</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
+			</div>
+		</div>
+		<div class="tile">
+			<div class="title">
+				Java Setup
+			</div>
+			<div class="thumb">
+				<img src="">
+			</div>
+			<div class="price">
+				295 DKK
+			</div>
+			<div class="info">
+				<button>INFO</button>
+			</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
+			</div>
+		</div>
+		<div class="tile">
+			<div class="title">
+				Java Setup
+			</div>
+			<div class="thumb">
+				<img src="">
+			</div>
+			<div class="price">
+				295 DKK
+			</div>
+			<div class="info">
+				<button>INFO</button>
+			</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
+			</div>
+		</div>
+		<div class="tile">
+			<div class="title">
+				Java Setup
+			</div>
+			<div class="thumb">
+				<img src="">
+			</div>
+			<div class="price">
+				295 DKK
+			</div>
+			<div class="info">
+				<button>INFO</button>
+			</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
+			</div>
+		</div>
+		<div class="tile">
+			<div class="title">
+				Java Setup
+			</div>
+			<div class="thumb">
+				<img src="">
+			</div>
+			<div class="price">
+				295 DKK
+			</div>
+			<div class="info">
+				<button>INFO</button>
+			</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
+			</div>
+		</div>
+		<div class="tile">
+			<div class="title">
+				Java Setup
+			</div>
+			<div class="thumb">
+				<img src="">
+			</div>
+			<div class="price">
+				295 DKK
+			</div>
+			<div class="info">
+				<button>INFO</button>
+			</div>
+			<div class="handshake">
+				<button>HANDSHAKE</button>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include "modules/modals/info.html";
+?>
+</body>
 </html>
