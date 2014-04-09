0a6951bcb3b89bc18f45a290336c53ca39203413
kalkun <jesper.henrichsen@gmail.com>
2014-04-09 21:58:31 +0200

    started /find

--- index.php
+++ index.php
@@ -1,41 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<title>ReBook</title>
-<link rel="shortcut icon" href="/resources/icons/logo.ico"/>
-<head>
-	<link rel="stylesheet" type="text/css" href="css/frontpage/frontpage.css">
-	<link rel="stylesheet" type="text/css" href="css/frontpage/general.css">
-	<link rel="stylesheet" type="text/css" href="css/frontpage/tile.css">
-</head>
+<?php
+	include "head.php";
+?>
 <body>
-	<div class="topbar">
-		<div class="home">
-			<div class="logo">
-			</div>
-			<div class="brand">
-				reBOOK
-			</div>
-		</div>
-		<div class="find">
-			FIND
-		</div>
-		<div class="sell">
-			SELL
-		</div>
-		<div class="about">
-			ABOUT
-		</div>
-		<div class="bugs">
-			BUGS
-		</div>
-		<input class="searchfield" type="text" placeholder="search">
-	</input>
-	<div class="user">
-		Jesper
-	</div>
-	<div class="edit">
-	</div>
-</div>
+	<?php
+		include "topbar.php";
+	?>
 <div class="banner">
 	<div>
 		<div class="header">
